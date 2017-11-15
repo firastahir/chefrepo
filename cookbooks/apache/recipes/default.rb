17beb2cadae213a5f54225b2a4b47c0aeedcebf8
firastahir <firas.tahir@gmail.com>
2017-11-15 07:00:39 +0000

    Uploading a master apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+# 
+package 'apache2' do
+ package_name 'httpd'
+end
+service 'apache2' do
+ service_name 'httpd'
+ action [:start, :enable]
+end
